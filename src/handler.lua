2660acc429e196c945d94c45085e7f7b3e3ac4cb
jeremyjpj0916 <31913027+jeremyjpj0916@users.noreply.github.com>
2019-01-02 10:16:01 -0500

    Further refactor for 1.0 compatibility
    Cleaned up per recommendation of bungle.

--- src/handler.lua
+++ src/handler.lua
@@ -7,14 +7,6 @@ local KongClusterDrain = BasePlugin:extend()
 KongClusterDrain.PRIORITY = 3 --The standard request termination plugin is 2 so we need to run before that and beat it out in priority.
 KongClusterDrain.VERSION = "1.1.0"
 
-local function flush(ctx)
-  ctx = ctx or ngx.ctx
-  local response = ctx.delayed_response
-  local status   = response.status_code
-  ngx.status = status
-  return ngx.exit(status)
-end
-
 function KongClusterDrain:new()
   KongClusterDrain.super.new(self, "kong-cluster-drain")
 end
@@ -23,17 +15,6 @@ function KongClusterDrain:access(conf)
  KongClusterDrain.super.access(self)
  
  if conf.hostname == host then --If host has been set check if match and start throwing http status for maintenance
-  local ctx = ngx.ctx
-  if ctx.delay_response and not ctx.delayed_response then
-    ctx.delayed_response = {
-      status_code  = 503
-    }
-
-    ctx.delayed_response_callback = flush
-
-    return
-  end
-
   return kong.response.exit(503, { message = "Scheduled Maintenance" })
  end
  
